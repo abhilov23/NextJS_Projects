99943c97ed3aa9f0c8b45ef3e05075a32cf4e0fc
Abhilov23 <abhilovgupta01@gmail.com>
2025-02-14 18:33:39 +0530

    deleted last app

--- mstrymessage/schemas/acceptMessageSchema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const acceptMessageSchema = z.object({
-    acceptMessages: z.boolean().default(true),
-    messageContent: z.string().min(10, { message: "Message content must be at least 10 characters" })
-});
--- mstrymessage/schemas/verifySchema.ts
+++
@@ -1,5 +0,0 @@
-import {z} from "zod";
-
-export const verifySchema = z.object({
-    acceptMessages: z.boolean()
-})
